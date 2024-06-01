e2ceefdfffc01fa41f62ebf9b59b7b119502dab3
KanyShigure <1713483284@qq.com>
2024-06-02 04:26:51 +0800

    初始化

--- src/main/java/org/verdurae/verduraeactionplan/VerduraeActionPlan.java
+++ src/main/java/org/verdurae/verduraeactionplan/VerduraeActionPlan.java
@@ -6,12 +6,11 @@ public final class VerduraeActionPlan extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
